742c44fd45d956fde7743f7b5b7551068633b51a
naomitechltd <naomitechltd@gmail.com>
2026-09-22 09:17:10 +0200

    Add Appwrite client setup and environment config

---
+++ src/lib/appwrite.js
@@ -0,0 +1,22 @@
+import { Client, Account, TablesDB, Storage, ID, Query } from "appwrite";
+
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+export const tablesDB = new TablesDB(client);
+export const storage = new Storage(client);
+
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID;
+export const SONGS_TABLE_ID = import.meta.env.VITE_APPWRITE_SONGS_TABLE_ID;
+export const LIKES_TABLE_ID = import.meta.env.VITE_APPWRITE_LIKES_TABLE_ID;
+export const PLAYLISTS_TABLE_ID = import.meta.env.VITE_APPWRITE_PLAYLISTS_TABLE_ID;
+export const PLAYLIST_SONGS_TABLE_ID = import.meta.env.VITE_APPWRITE_PLAYLIST_SONGS_TABLE_ID;
+
+export { ID, Query };
+
+export function fileUrl(fileId) {
+  return `${import.meta.env.VITE_APPWRITE_ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${fileId}/view?project=${import.meta.env.VITE_APPWRITE_PROJECT_ID}`;
+}
